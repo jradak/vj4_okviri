d3f5621c033747e64c2148dadf7ed7778c6425d8
Josipa Radak <jradak@pmfst.hr>
2021-10-31 12:17:18 +0100

    http poruke

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http')
+let poruke = [
+ {
+ id: 1,
+ sadrzaj: 'HTML je jednostavan',
+ },
+ {
+ id: 2,
+ sadrzaj: 'React koristi JSX sintaksu',
+ },
+ {
+ id: 3,
+ sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+ }
+]
 const app = http.createServer((req, res) =>{
- res.writeHead(200, {'Content-Type': 'text/plain'})
- res.end('Pozdrav od posluzitelja')
+ res.writeHead(200, {'Content-Type': 'application/json'})
+ res.end(JSON.stringify(poruke))
 })
 const PORT = 3001
 app.listen(PORT)
