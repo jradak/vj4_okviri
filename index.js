e198302ec74695ae5cf2d65e4e547acd6057fba0
Josipa Radak <jradak@pmfst.hr>
2021-10-31 12:37:51 +0100

    postavljanje servera preko expressa i get zahtjev za poruke

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 let poruke = [
- {
- id: 1,
- sadrzaj: 'HTML je jednostavan',
- },
- {
- id: 2,
- sadrzaj: 'React koristi JSX sintaksu',
- },
- {
- id: 3,
- sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
- }
+    {
+    id: 1,
+    sadrzaj: 'HTML je jednostavan',
+    },
+    {
+    id: 2,
+    sadrzaj: 'React koristi JSX sintaksu',
+    },
+    {
+    id: 3,
+    sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+    }
 ]
-const app = http.createServer((req, res) =>{
- res.writeHead(200, {'Content-Type': 'application/json'})
- res.end(JSON.stringify(poruke))
+   
+app.get('/', (req, res) =>{
+ res.send('<h1>Pozdrav od Express servera</h1>')
+})
+app.get('/api/poruke', (req, res) =>{
+ res.json(poruke)
 })
 const PORT = 3001
-app.listen(PORT)
-console.log(`Posluzitelj je pokrenut na portu ${PORT}`);
+app.listen(PORT, () => {
+ console.log(`Posluzitelj je pokrenut na portu ${PORT}`);
+})
