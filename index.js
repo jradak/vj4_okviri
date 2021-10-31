b38b4b99e6750920f6db6b75ab8a93ec6d1fb76b
Josipa Radak <jradak@pmfst.hr>
2021-10-31 12:11:52 +0100

    http server 1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("test")
+const http = require('http')
+const app = http.createServer((req, res) =>{
+ res.writeHead(200, {'Content-Type': 'text/plain'})
+ res.end('Pozdrav od posluzitelja')
+})
+const PORT = 3001
+app.listen(PORT)
+console.log(`Posluzitelj je pokrenut na portu ${PORT}`);
